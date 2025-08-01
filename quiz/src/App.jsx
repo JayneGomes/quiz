f482a74d5fa0e8a4ada94435c302c518f69fed19
Jayne Gomes <jaygmorais@gmail.com>
2025-07-31 22:41:51 -0300

    chore: initial project setup

---
+++ quiz/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <>
+      <h1>Quiz de Programação</h1>
+      <Welcome />
+    </>
+  );
+}
+
+export default App;
